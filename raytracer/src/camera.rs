c402ab9b33ba1092004b6692aeb937f8f8b5b1c7
David Singleton <davidsingleton@gmail.com>
2021-12-28 23:42:19 +0000

    JSON serialization / deserialization

--- raytracer/src/camera.rs
+++ raytracer/src/camera.rs
@@ -1,16 +1,44 @@
+use serde::{Deserialize, Serialize};
+
 use crate::point3d::Point3D;
 use crate::ray::Ray;
 
 #[cfg(test)]
 use assert_approx_eq::assert_approx_eq;
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, Serialize, Deserialize)]
+#[serde(from = "CameraParams")]
 pub struct Camera {
-    pub origin: Point3D,
+    #[serde(skip_serializing)]
+    pub origin: Point3D, // Note, don't serialize any of the computed fields.
+    #[serde(skip_serializing)]
     pub lower_left_corner: Point3D,
+    #[serde(skip_serializing)]
     pub focal_length: f64,
+    #[serde(skip_serializing)]
     pub horizontal: Point3D,
+    #[serde(skip_serializing)]
     pub vertical: Point3D,
+    look_from: Point3D,
+    look_at: Point3D,
+    vup: Point3D,
+    vfov: f64, // vertical field-of-view in degrees
+    aspect: f64,
+}
+
+#[derive(Debug, Clone, Copy, Serialize, Deserialize)]
+pub struct CameraParams {
+    pub look_from: Point3D,
+    pub look_at: Point3D,
+    pub vup: Point3D,
+    pub vfov: f64, // vertical field-of-view in degrees
+    pub aspect: f64,
+}
+
+impl From<CameraParams> for Camera {
+    fn from(p : CameraParams) -> Self {
+        Camera::new(p.look_from, p.look_at, p.vup, p.vfov, p.aspect)
+    }
 }
 
 impl Camera {
@@ -40,6 +68,11 @@ impl Camera {
             focal_length: (look_from - look_at).length(),
             horizontal,
             vertical,
+            look_from,
+            look_at,
+            vup,
+            vfov,
+            aspect,
         }
     }
 
@@ -86,4 +119,23 @@ fn test_camera_get_ray() {
     assert_approx_eq!(ray.direction.x(), (2.0 / 3.0));
     assert_approx_eq!(ray.direction.y(), -(2.0 / 3.0));
     assert_approx_eq!(ray.direction.z(), -(1.0 / 3.0));
+}
+
+#[test]
+fn test_to_json() {
+    let camera = Camera::new(
+        Point3D::new(-4.0, 4.0, 1.0),
+        Point3D::new(0.0, 0.0, -1.0),
+        Point3D::new(0.0, 1.0, 0.0),
+        160.0,
+        (800 / 600) as f64,
+    );
+    let serialized = serde_json::to_string(&camera).unwrap();
+    assert_eq!("{\"look_from\":{\"x\":-4.0,\"y\":4.0,\"z\":1.0},\"look_at\":{\"x\":0.0,\"y\":0.0,\"z\":-1.0},\"vup\":{\"x\":0.0,\"y\":1.0,\"z\":0.0},\"vfov\":160.0,\"aspect\":1.0}", serialized);
+    let c = serde_json::from_str::<Camera>(&serialized).unwrap();
+    assert_eq!(camera.origin, c.origin);
+    assert_eq!(camera.lower_left_corner, c.lower_left_corner);
+    assert_eq!(camera.focal_length, c.focal_length);
+    assert_eq!(camera.horizontal, c.horizontal);
+    assert_eq!(camera.vertical, c.vertical);
 }
--- raytracer/src/sphere.rs
+++ raytracer/src/sphere.rs
@@ -1,3 +1,5 @@
+use serde::{Deserialize, Serialize};
+
 use crate::materials::Material;
 use crate::point3d::Point3D;
 use crate::ray::HitRecord;
@@ -7,9 +9,11 @@ use crate::ray::Ray;
 #[cfg(test)]
 use crate::materials::Lambertian;
 #[cfg(test)]
+use crate::materials::Texture;
+#[cfg(test)]
 use palette::Srgb;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Deserialize, Serialize)]
 pub struct Sphere {
     center: Point3D,
     radius: f64,
@@ -85,4 +89,53 @@ fn test_sphere_hit() {
     let ray = Ray::new(Point3D::new(0.0, 0.0, -5.0), Point3D::new(0.0, 0.0, 1.0));
     let hit = sphere.hit(&ray, 0.0, f64::INFINITY);
     assert_eq!(hit.unwrap().t, 4.0);
+}
+
+#[test]
+fn test_to_json() {
+    let sphere = Sphere::new(
+        Point3D::new(0.0, 0.0, 0.0),
+        1.0,
+        Material::Lambertian(Lambertian::new(Srgb::new(
+            0.5 as f32, 0.5 as f32, 0.5 as f32,
+        ))),
+    );
+    let serialized = serde_json::to_string(&sphere).unwrap();
+    assert_eq!(
+        "{\"center\":{\"x\":0.0,\"y\":0.0,\"z\":0.0},\"radius\":1.0,\"material\":{\"Lambertian\":{\"albedo\":[0.5,0.5,0.5]}}}",
+        serialized,
+    );
+    let s = serde_json::from_str::<Sphere>(&serialized).unwrap();
+    assert_eq!(sphere.center, s.center);
+    assert_eq!(sphere.radius, s.radius);
+
+    let textured_sphere = Sphere::new(
+        Point3D::new(0.0, 0.0, 0.0),
+        1.0,
+        Material::Texture(Texture::new(
+            Srgb::new(0.5 as f32, 0.5 as f32, 0.5 as f32),
+            "data/earth.jpg",
+            0.0,
+        )),
+    );
+
+    let tserialized = serde_json::to_string(&textured_sphere).unwrap();
+    assert_eq!(
+        "{\"center\":{\"x\":0.0,\"y\":0.0,\"z\":0.0},\"radius\":1.0,\"material\":{\"Texture\":{\"albedo\":[0.5,0.5,0.5],\"pixels\":\"/tmp/texture.jpg\",\"width\":2048,\"height\":1024,\"h_offset\":0.0}}}",
+        tserialized,
+    );
+
+    let tex = Texture::new(
+        Srgb::new(0.5 as f32, 0.5 as f32, 0.5 as f32),
+        "data/earth.jpg",
+        0.0,
+    );
+    let tloadable = "{\"center\":{\"x\":0.0,\"y\":0.0,\"z\":0.0},\"radius\":1.0,\"material\":{\"Texture\":{\"albedo\":[0.5,0.5,0.5],\"pixels\":\"data/earth.jpg\",\"width\":2048,\"height\":1024,\"h_offset\":0.0}}}";
+    let loaded = serde_json::from_str::<Sphere>(&tloadable).unwrap();
+    match loaded.material {
+        Material::Texture(ref t) => {
+            assert_eq!(t.pixels, tex.pixels);
+        }
+        _ => panic!("Wrong material type"),
+    }
 }
